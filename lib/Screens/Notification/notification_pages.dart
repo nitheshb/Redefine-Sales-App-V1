564ae5327af5c05c34d98df1ed9f8db9d59fa86b
HarshKothari88 <techsmash88@gmail.com>
2022-09-21 08:13:09 +0530

    new

--- lib/Screens/Notification/notification_pages.dart
+++ lib/Screens/Notification/notification_pages.dart
@@ -1,12 +1,17 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:redefineerp/Screens/Notification/notification_controller.dart';
 import 'package:redefineerp/themes/themes.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 class NotificationPage extends StatelessWidget {
   const NotificationPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put<NotificatonController>(NotificatonController());
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -29,53 +34,112 @@ class NotificationPage extends StatelessWidget {
               ))
         ],
       ),
-      body: SingleChildScrollView(
-        physics: const BouncingScrollPhysics(),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
-              child: Text(
-                'Today',
-                style: Get.theme.kNormalStyle
-                    .copyWith(color: Get.theme.btnTextCol.withOpacity(0.6)),
-              ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+            child: Text(
+              'Today',
+              style: Get.theme.kNormalStyle
+                  .copyWith(color: Get.theme.btnTextCol.withOpacity(0.6)),
             ),
-            Container(
-              width: Get.size.width,
-              height: 2,
-              color: Get.theme.btnTextCol.withOpacity(0.2),
-            ),
-            notificationCard(
-                title: 'Mahendra Mangal',
-                messageType: 'Completed: ',
-                message:
-                    "Collect insurance documents from Mr.Rajesh Sales Plan for New Product",
-                time: "5 Min ago."),
-            notificationCard(
-                title: 'Vivek Dhillon',
-                messageType: 'Assigned: ',
-                message:
-                    "Collect insurance documents from Mr.Rajesh Sales Plan for New Product",
-                time: "1 Hour ago."),
-            notificationCard(
-                title: 'Mahendra Mangal',
-                messageType: 'Commented on: ',
-                message: "Check Screen for Todo",
-                mentionText: '@nitesh Please make sure that you’re collecting ',
-                time: "5 Min ago."),
-            notificationCard(
-                title: 'Overdue!',
-                messageType: 'Alert: ',
-                warning: true,
-                message:
-                    "Collect insurance documents from Mr.Rajesh Sales Plan for New Product",
-                time: "Yesterday"),
-          ],
-        ),
+          ),
+          Container(
+            width: Get.size.width,
+            height: 2,
+            color: Get.theme.btnTextCol.withOpacity(0.2),
+          ),
+          Expanded(child: streamNotifications()),
+          // notificationCard(
+          //     title: 'Mahendra Mangal',
+          //     messageType: 'Completed: ',
+          //     message:
+          //         "Collect insurance documents from Mr.Rajesh Sales Plan for New Product",
+          //     time: "5 Min ago."),
+          // notificationCard(
+          //     title: 'Vivek Dhillon',
+          //     messageType: 'Assigned: ',
+          //     message:
+          //         "Collect insurance documents from Mr.Rajesh Sales Plan for New Product",
+          //     time: "1 Hour ago."),
+          // notificationCard(
+          //     title: 'Mahendra Mangal',
+          //     messageType: 'Commented on: ',
+          //     message: "Check Screen for Todo",
+          //     mentionText: '@nitesh Please make sure that you’re collecting ',
+          //     time: "5 Min ago."),
+          // notificationCard(
+          //     title: 'Overdue!',
+          //     messageType: 'Alert: ',
+          //     warning: true,
+          //     message:
+          //         "Collect insurance documents from Mr.Rajesh Sales Plan for New Product",
+          //     time: "Yesterday"),
+        ],
       ),
     );
+  }
+
+  Widget streamNotifications() {
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('spark_assignedTasks')
+            .where("to_email",
+                isEqualTo: FirebaseAuth.instance.currentUser?.email)
+            // .where("due_date",
+            //     isEqualTo: "${DateTime.now().microsecondsSinceEpoch - } ")
+            // .where("status", isEqualTo: "InProgress")
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text("Something went wrong! 😣..."),
+            );
+          } else if (snapshot.hasData) {
+            print('no of notifi is ${snapshot.data?.docs.length}');
+            // numOfTodayTasks.value = snapshot.data!.docs.length;
+            return ListView.builder(
+                shrinkWrap: true,
+                physics: const BouncingScrollPhysics(),
+                itemCount: snapshot.data?.docs.length,
+                itemBuilder: (context, index) {
+                  late QueryDocumentSnapshot<Object?>? taskData =
+                      snapshot.data?.docs[index];
+                  final now = DateTime.now();
+                  final difference = now.difference(
+                      DateTime.fromMillisecondsSinceEpoch(
+                          taskData!['created_on']));
+                  if (snapshot.data!.docs == 0) {
+                    return const Center(
+                      child: Text("Notifications Loading..."),
+                    );
+                  } else {
+                    return notificationCard(
+                        title: taskData['by_name'],
+                        messageType: taskData['status'] == 'Done'
+                            ? 'Completed: '
+                            : 'Assigned: ',
+                        message: taskData['task_desc'],
+                        time: timeago.format(now.subtract(difference)));
+                  }
+                });
+          } else {
+            return Center(
+              child: Column(
+                children: const [
+                  Center(
+                    child: CircularProgressIndicator(),
+                  ),
+                  SizedBox(height: 50),
+                  Center(
+                    child: Text("Notifications Loading..."),
+                  )
+                ],
+              ),
+            );
+          }
+        });
   }
 
   Widget notificationCard(
