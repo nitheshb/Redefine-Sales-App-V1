33c887ed0a64b54ad79c9e4ca23e2f58c05a04c7
Satyam Jha <Satyamjha9911@gmail.com>
2023-05-11 20:09:06 +0530

    No of completed task in graph in dashboard

--- lib/Screens/Dashboard/dashboard_pages.dart
+++ lib/Screens/Dashboard/dashboard_pages.dart
@@ -4,16 +4,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter_feather_icons/flutter_feather_icons.dart';
 import 'package:get/get.dart';
 import 'package:redefineerp/Screens/Dashboard/dashboard_controller.dart';
-import 'package:redefineerp/Screens/Notification/notification_controller.dart';
+// import 'package:redefineerp/Screens/Notification/notification_controller.dart';
 import 'package:redefineerp/Utilities/dummyData.dart';
-import 'package:redefineerp/helpers/firebase_help.dart';
+// import 'package:redefineerp/helpers/firebase_help.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 import 'package:redefineerp/themes/constant.dart';
 import 'package:redefineerp/themes/container.dart';
 import 'package:redefineerp/themes/spacing.dart';
 import 'package:redefineerp/themes/textFile.dart';
 import 'package:redefineerp/themes/themes.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
-import 'package:timeago/timeago.dart' as timeago;
+//import 'package:timeago/timeago.dart' as timeago;
 
 class DashboardPage extends StatefulWidget {
   const DashboardPage({Key? key}) : super(key: key);
@@ -23,6 +24,7 @@ class DashboardPage extends StatefulWidget {
 }
 
 class _DashboardPageState extends State<DashboardPage> {
+  DashboardController dashboardController = Get.put(DashboardController());
   late List<String> filterTime = [
     "All time",
     "Yesterday",
@@ -30,6 +32,13 @@ class _DashboardPageState extends State<DashboardPage> {
     "7 days ago"
   ];
 
+  num today = 0;
+  num day2 = 0;
+  num day3 = 0;
+  num day4 = 0;
+  num day5 = 0;
+  num day6 = 0;
+  num day7 = 0;
   late String time = "All time";
 
   var currentUserId = FirebaseAuth.instance.currentUser!.uid;
@@ -54,35 +63,37 @@ class _DashboardPageState extends State<DashboardPage> {
 
     chartData = <ChartSampleData>[
       ChartSampleData(
-          x: '1', y: 10, pointColor: Constant.softColors.blue.color),
+          x: '${DateTime.now().day}',
+          y: today,
+          pointColor: Constant.softColors.blue.color),
       ChartSampleData(
-        x: '2',
-        y: 8,
+        x: '${DateTime.now().day + 1}',
+        y: day2,
         pointColor: Constant.softColors.violet.color,
       ),
       ChartSampleData(
-        x: '3',
-        y: 16,
+        x: '${DateTime.now().day + 2}',
+        y: day3,
         pointColor: Constant.softColors.orange.color,
       ),
       ChartSampleData(
-        x: '4',
-        y: 24,
+        x: '${DateTime.now().day + 3}',
+        y: day4,
         pointColor: Constant.softColors.blue.color,
       ),
       ChartSampleData(
-        x: '5',
-        y: 32,
+        x: '${DateTime.now().day + 4}',
+        y: day5,
         pointColor: Constant.softColors.orange.color,
       ),
       ChartSampleData(
-        x: '6',
-        y: 30,
+        x: '${DateTime.now().day + 5}',
+        y: day6,
         pointColor: Constant.softColors.blue.color,
       ),
       ChartSampleData(
-        x: '7',
-        y: 25,
+        x: '${DateTime.now().day + 6}',
+        y: day7,
         pointColor: Constant.softColors.violet.color,
       ),
     ];
@@ -168,7 +179,7 @@ class _DashboardPageState extends State<DashboardPage> {
                 controller.time,
               ),
               FxSpacing.width(8),
-              Icon(
+              const Icon(
                 FeatherIcons.chevronDown,
                 size: 14,
               )
@@ -234,13 +245,40 @@ class _DashboardPageState extends State<DashboardPage> {
                 .snapshots(),
             builder: (context, snapshot) {
               if (snapshot.hasError) {
-                return Center(child: Text("Something went wrong"));
+                return const Center(child: Text("Something went wrong"));
               } else if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(
+                return const Center(
                   child: CircularProgressIndicator(),
                 );
               } else if (snapshot.hasData) {
-                print("completed======>${snapshot.data!.docs}");
+                for (var i = 0; i < snapshot.data!.docs.length; i++) {
+                  if (snapshot.data!.docs[i]['completedOn'] ==
+                      DateTime.now().day) {
+                    today = today + 1;
+                  } else if (snapshot.data!.docs[i]['completedOn'] ==
+                      DateTime.now().day + 1) {
+                    day2 = day2 + 1;
+                  } else if (snapshot.data!.docs[i]['completedOn'] ==
+                      DateTime.now().day + 2) {
+                    day3 = day3 + 1;
+                  } else if (snapshot.data!.docs[i]['completedOn'] ==
+                      DateTime.now().day + 3) {
+                    day4 = day4 + 1;
+                  } else if (snapshot.data!.docs[i]['completedOn'] ==
+                      DateTime.now().day + 4) {
+                    day5 = day5 + 1;
+                  } else if (snapshot.data!.docs[i]['completedOn'] ==
+                      DateTime.now().day + 5) {
+                    day6 = day6 + 1;
+                  } else if (snapshot.data!.docs[i]['completedOn'] ==
+                      DateTime.now().day + 6) {
+                    day7 = day7 + 1;
+                  }
+                }
+
+                print(today);
+                initChartData();
+                // print("Completed tasks ${snapshot.data!.docs[1].data()}");
               }
 
               return Expanded(
@@ -294,9 +332,9 @@ class _DashboardPageState extends State<DashboardPage> {
                 .snapshots(),
             builder: (context, snapshot) {
               if (snapshot.hasError) {
-                return Center(child: Text("Something went wrong"));
+                return const Center(child: Text("Something went wrong"));
               } else if (snapshot.connectionState == ConnectionState.waiting) {
-                return Center(
+                return const Center(
                   child: CircularProgressIndicator(),
                 );
               } else if (snapshot.hasData) {
@@ -356,29 +394,74 @@ class _DashboardPageState extends State<DashboardPage> {
     return FxContainer(
         child: Column(
       children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Row(
+        StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('spark_assignedTasks')
+              .where("status", isEqualTo: "completed")
+              .where("to_uid",
+                  isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Center(child: Text("Something went wrong"));
+            } else if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasData) {
+              initChartData();
+            }
+
+            return Column(
               children: [
-                FxContainer(
-                  width: 10,
-                  height: 20,
-                  color: Get.theme.primaryContainer,
-                  borderRadiusAll: 2,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        FxContainer(
+                          width: 10,
+                          height: 20,
+                          color: Get.theme.primaryContainer,
+                          borderRadiusAll: 2,
+                        ),
+                        FxSpacing.width(8),
+                        FxText.titleSmall(
+                          "Completion Ranking",
+                          fontWeight: 600,
+                        ),
+                      ],
+                    ),
+                    timeFilter()
+                  ],
                 ),
-                FxSpacing.width(8),
-                FxText.titleSmall(
-                  "Completion Ranking",
-                  fontWeight: 600,
-                ),
+                FxSpacing.height(20),
+                StreamBuilder<QuerySnapshot>(
+                  stream: FirebaseFirestore.instance
+                      .collection('spark_assignedTasks')
+                      .where("status", isEqualTo: "completed")
+                      .where("to_uid",
+                          isEqualTo: FirebaseAuth.instance.currentUser!.uid)
+                      .snapshots(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasError) {
+                      return const Center(child: Text("Something went wrong"));
+                    } else if (snapshot.connectionState ==
+                        ConnectionState.waiting) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    } else if (snapshot.hasData) {
+                      initChartData();
+                    }
+
+                    return salesStatusChart();
+                  },
+                )
               ],
-            ),
-            timeFilter()
-          ],
-        ),
-        FxSpacing.height(20),
-        salesStatusChart()
+            );
+          },
+        )
       ],
     ));
   }
@@ -390,16 +473,17 @@ class _DashboardPageState extends State<DashboardPage> {
       margin: EdgeInsets.zero,
       plotAreaBorderWidth: 0,
       primaryXAxis: CategoryAxis(
-        majorGridLines: MajorGridLines(
+        majorGridLines: const MajorGridLines(
           width: 0,
           color: Colors.transparent,
         ),
       ),
       primaryYAxis: NumericAxis(
-          majorGridLines: MajorGridLines(width: 0),
-          axisLine: AxisLine(width: 0, color: Colors.transparent),
-          labelFormat: '{value}k',
-          majorTickLines: MajorTickLines(size: 4, color: Colors.transparent)),
+          majorGridLines: const MajorGridLines(width: 0),
+          axisLine: const AxisLine(width: 0, color: Colors.transparent),
+          labelFormat: '{value}',
+          majorTickLines:
+              const MajorTickLines(size: 4, color: Colors.transparent)),
       series: _getDefaultColumnSeries(),
       tooltipBehavior: tooltipBehavior,
     );
@@ -417,7 +501,7 @@ class _DashboardPageState extends State<DashboardPage> {
         width: 0.5,
         borderRadius: BorderRadius.vertical(
             top: Radius.circular(Constant.containerRadius.xs)),
-        dataLabelSettings: DataLabelSettings(
+        dataLabelSettings: const DataLabelSettings(
             isVisible: false, textStyle: TextStyle(fontSize: 10)),
       )
     ];
